ca0a526f27824a16facdb172821addef87c0ef90
3Disney <victorcauzz@hotmail.com>
2023-01-07 20:33:00 -0300

    Tirei o lights2D

---
+++ Assets/Disney/Corvo/CrawAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawAI : MonoBehaviour
+{
+    // Components
+
+    // Enemy parameters (Craw)
+
+    // Enemy animation states
+    Animator animator;
+    string currentState;
+    const string ENEMY_IDLE = "Enemy_Idle";
+    const string ENEMY_RUN = "Enemy_Run";
+    const string ENEMY_JUMP = "Enemy_Jump";
+    const string ENEMY_FALL = "Enemy_Fall";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
