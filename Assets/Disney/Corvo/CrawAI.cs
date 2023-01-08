fe73cd68dcc4d3059aae632e82d03bea7f503ffe
3Disney <victorcauzz@hotmail.com>
2023-01-07 21:46:56 -0300

    Inicio do controle do corvo+ otimização da movimentação do player

--- Assets/Disney/Corvo/CrawAI.cs
+++ Assets/Disney/Corvo/CrawAI.cs
@@ -5,25 +5,68 @@ using UnityEngine;
 public class CrawAI : MonoBehaviour
 {
     // Components
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
 
     // Enemy parameters (Craw)
 
-    // Enemy animation states
-    Animator animator;
-    string currentState;
-    const string ENEMY_IDLE = "Enemy_Idle";
-    const string ENEMY_RUN = "Enemy_Run";
-    const string ENEMY_JUMP = "Enemy_Jump";
-    const string ENEMY_FALL = "Enemy_Fall";
+    // State Machine:
+    private enum state
+    {
+        fly, dive, flee
+    }
+   state currentState;
+    string[] strStates = {"Enemy_Fly","Enemy_Dive","Enemy_Flee"}; //vetor de string states
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // getting components
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		animator = gameObject.GetComponent<Animator>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+        // setting initial status
+		currentState = state.fly;
+		animator.Play(strStates[(int)state.fly]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		DoTheFlip();
+		MoveEnemy();
     }
+
+    // Change State
+    private void ChangeState(state newState)
+    {
+        if (newState == currentState)
+            return;
+        //if (for diferente de null blablabla) (no nosso nao precisa)
+        animator.Play(strStates[(int)state.dive]);
+
+        currentState = newState;
+    }
+
+    // Verifica se determinada animação rodou (só serve para animações de uso único, então talvez não seja necessário)
+    bool IsAnimationPlaying(Animator animator, string stateName)
+    {
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            return true;
+        else return false;
+    }
+
+    void DoTheFlip()
+    {
+
+    }
+
+    void MoveEnemy()
+    [
+
+    ]
 }
--- Assets/Thor/Scripts/Player/playerMovement.cs
+++ Assets/Thor/Scripts/Player/playerMovement.cs
@@ -7,11 +7,14 @@ public class playerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed;
     public Animator animator;
+    SpriteRenderer sprite;
     private Vector2 moveDirection;
     private GameObject cam;
     void Start()
     {
+        //Components
         cam = GameObject.FindWithTag("MainCamera");
+        sprite = gameObject.GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void Update()
@@ -30,13 +33,13 @@ public class playerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
         if(moveX > 0)
         {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            sprite.flipX = false;
             //gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
             //cam.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         else if(moveX < 0)
         {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            sprite.flipX = true;
             //gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
             //cam.transform.rotation *= Quaternion.Euler(0, 180, 0);
         }
