508c8e10e4b2fbbbd711571e7942c25a51bf8915
Mario Bezerra <mariocelio102@gmail.com>
2023-01-07 21:31:37 -0400

    camera movement with mouse

--- Assets/Thor/Scripts/Player/FollowPlayer.cs
+++ Assets/Thor/Scripts/Player/FollowPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     private GameObject player;
+    [SerializeField]
+    private float followSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,12 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = -10f;
+
+        Vector3 finalPos = (playerPos + mousePos) / 2f;
+
+        transform.position = Vector3.Slerp(transform.position, finalPos, followSpeed * Time.deltaTime);
     }
 }
