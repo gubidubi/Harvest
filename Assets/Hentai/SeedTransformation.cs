954296efcd60903d5a6412de63b710526ed01d9d
Emanuel Mendes Monteiro <emanuelmendes2001@gmail.com>
2023-01-09 01:19:48 -0300

    atiradeira

--- Assets/Hentai/SeedTransformation.cs
+++ Assets/Hentai/SeedTransformation.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class SeedTransformation : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject prefeb;
+    public GameObject prefab;
+    public Health health;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(health.health == health.maxHealth)
+        {
+            seedTransformation();
+        }
     }
 
     public void seedTransformation()
     {
         Destroy(gameObject);
-        Instantiate(prefeb);
+        Instantiate(prefab, transform.position, Quaternion.identity);
     }
 
 }
